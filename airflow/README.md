1a58b4c371d72bf01e266fc478eef0acebeb8b8e
Drunkar <drunkars.p@gmail.com>
2015-06-29 21:11:34 +0900

    Use CeleryExecutor and MySQL broker.

--- airflow/README.md
+++ airflow/README.md
@@ -1,15 +1,21 @@
 # airflow
 dockerfile for airbnb/airflow ( https://github.com/airbnb/airflow )
 
-```
-sudo docker run --name airflow -p 8080:8080 -d drunkar/airflow
-```
+Use CeleryExecutor and MySQL as its backend.
 
-or
+## Usage
 
+1. Run MySQL Container.
 
-```
-sudo docker run --name airflow -v /your/working/dir:/airflow -p 8080:8080 -d drunkar/airflow
-```
+  ```
+sudo docker run --name airflow-db -e MYSQL_ROOT_PASSWORD=airflow-db -d -v /var/lib/mysql:/var/lib/mysql mysql
+  ```
+
+2. Run Airflow container.
+
+  ```
+sudo docker run --name airflow --link airflow-db:mysql -v /docker_volumes/airflow/dags:/airflow/dags -v /docker_volumes/airflow/logs:/airflow/logs -p 8080:8080 -p 5555:5555 -d drunkar/airflow
+
+  ```
 
 Then you can access to ``http://localhost:8080``.
---
+++ airflow/entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+CMD="airflow webserver -p 8080"
+
+echo "
+[core]
+airflow_home = /airflow
+dags_folder = /airflow/dags
+base_log_folder = /airflow/logs
+executor = CeleryExecutor
+sql_alchemy_conn = mysql://root:${MYSQL_ENV_MYSQL_ROOT_PASSWORD}@${MYSQL_PORT_3306_TCP_ADDR}:3306/airflow
+parallelism = 32
+load_examples = True
+
+[webserver]
+base_url = http://localhost:8080
+web_server_host = 0.0.0.0
+web_server_port = 8080
+
+[smtp]
+smtp_host = localhost
+smtp_user = airflow
+smtp_port = 25
+smtp_password = airflow
+smtp_mail_from = airflow@airflow.com
+
+[celery]
+celery_app_name = airflow.executors.celery_executor
+celeryd_concurrency = 16
+worker_log_server_port = 8793
+broker_url = sqla+mysql://root:${MYSQL_ENV_MYSQL_ROOT_PASSWORD}@${MYSQL_PORT_3306_TCP_ADDR}:3306/airflow
+;broker_url = amqp://guest:guest@${RABBIT_PORT_5672_TCP_ADDR}:5672/airflow
+celery_result_backend = db+mysql://root:${MYSQL_ENV_MYSQL_ROOT_PASSWORD}@${MYSQL_PORT_3306_TCP_ADDR}:3306/airflow
+;celery_result_backend = amqp://guest:guest@${RABBIT_PORT_5672_TCP_ADDR}:5672/airflow
+flower_port = 8383
+default_queue = default
+
+[scheduler]
+job_heartbeat_sec = 5
+scheduler_heartbeat_sec = 5
+" \
+>> $AIRFLOW_HOME/airflow.cfg
+
+
+airflow initdb
+airflow worker &
+exec $CMD
