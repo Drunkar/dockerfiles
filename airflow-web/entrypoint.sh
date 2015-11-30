969d652db492a17cdde6e9920cc89ac3a3138109
Drunkar <drunkars.p@gmail.com>
2015-11-30 12:10:24 +0900

    Change parallelism.

--- airflow-web/entrypoint.sh
+++ airflow-web/entrypoint.sh
@@ -9,7 +9,7 @@ dags_folder = /airflow/dags
 base_log_folder = /airflow/logs
 executor = CeleryExecutor
 sql_alchemy_conn = mysql://root:${MYSQL_ENV_MYSQL_ROOT_PASSWORD}@${MYSQL_PORT_3306_TCP_ADDR}:3306/airflow
-parallelism = 100
+parallelism = 60
 load_examples = False
 
 [webserver]
@@ -26,7 +26,7 @@ smtp_mail_from = airflow@airflow.com
 
 [celery]
 celery_app_name = airflow.executors.celery_executor
-celeryd_concurrency = 200
+celeryd_concurrency = 60
 worker_log_server_port = 8793
 broker_url = sqla+mysql://root:${MYSQL_ENV_MYSQL_ROOT_PASSWORD}@${MYSQL_PORT_3306_TCP_ADDR}:3306/airflow
 ;broker_url = amqp://guest:guest@${RABBIT_PORT_5672_TCP_ADDR}:5672/airflow
