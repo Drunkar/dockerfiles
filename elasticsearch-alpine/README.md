e35a4d38d899950691ab6ceb929d40289ff6a030
Drunkar <drunkars.p@gmail.com>
2016-12-09 21:01:57 +0900

    Fix elasticsearch5 related problems.

--- elasticsearch-alpine/README.md
+++ elasticsearch-alpine/README.md
@@ -1,2 +1,22 @@
 # elasticsearch-alpine
 elasticsearch on alpine.
+
+## Usage
+
+* in your host pc, run below command:
+
+```
+sudo sysctl -w vm.max_map_count=262144
+```
+
+* then run container
+
+```
+docker run -d  -p 9200:9200 -v <path_you_like>:/usr/share/elasticsearch/data drunkar/elasticsearch-alpine
+```
+
+* if on ec2 t2.micro instance
+
+```
+docker run -d  -p 9200:9200 -e ES_JAVA_OPTS="-Xmx512m -Xms512m" -v <path_you_like>:/usr/share/elasticsearch/data drunkar/elasticsearch-alpine
+```
--- elasticsearch-alpine/docker-entrypoint.sh
+++ elasticsearch-alpine/docker-entrypoint.sh
@@ -1,20 +1,23 @@
-#!/bin/bash
+#!/bin/sh
+
+# ElasticSearch Docker Entrypoint as per:
+# https://github.com/docker-library/elasticsearch
+# Apache Licensed: https://github.com/docker-library/elasticsearch/blob/dce83166a2636abfac110a4555cdf17fd554ae91/LICENSE
 
 set -e
 
 # Add elasticsearch as command if needed
 if [ "${1:0:1}" = '-' ]; then
-	set -- elasticsearch "$@"
+  set -- elasticsearch "$@"
 fi
 
 # Drop root privileges if we are running elasticsearch
 # allow the container to be started with `--user`
 if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
-	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+  # Change the ownership of /usr/share/elasticsearch/data to elasticsearch
+  chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
 
-	set -- gosu elasticsearch "$@"
-	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
+  set -- gosu elasticsearch "$@"
 fi
 
 # As argument is not related to elasticsearch,
